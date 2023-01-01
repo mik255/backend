a9227152732ff2e822edcc527a194985d7c59eec
Mik255 <mikael20informatica@gmail.com>
2023-01-01 20:43:26 -0300

    update

--- lib/header.dart
+++ lib/header.dart
@@ -2,7 +2,8 @@ class Header {
   static get header => {
         'content-type': 'application/json',
         'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Headers': "Content-Type",
-        'Referrer-Policy': "no-referrer-when-downgrade"
+        'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept",
+        'Referrer-Policy': "no-referrer-when-downgrade",
+        "Access-Control-Allow-Methods": "GET,PUT,PATCH,POST,DELETE"
       };
 }
